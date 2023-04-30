0024702a1735a8e25888f2fbbc490f502ba55016
talle0 <chung.jibum@gmail.com>
2023-04-30 01:47:59 -0400

    새 핼로
    test 1

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello test 1'
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
-#def print_hi(name):
-#    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
