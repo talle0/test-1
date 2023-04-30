a0e06ade541ab6983808ada6e2f24ead9c1046d8
talle0 <chung.jibum@gmail.com>
2023-04-30 02:30:05 -0400

    Update 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Cloud Run")
     return "Hello {}!".format(name)
 
 if __name__ == "__main__":
